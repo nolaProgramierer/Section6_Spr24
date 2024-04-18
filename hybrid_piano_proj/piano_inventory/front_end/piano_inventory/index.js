57d3a7ab381c7896608fe006a62cb6788b1ef291
Glenn Langdon <glennlangdon@gmail.com>
2024-04-17 19:42:31 -0500

    babel, webpack config, React

--- hybrid_piano_proj/piano_inventory/front_end/piano_inventory/index.js
+++ hybrid_piano_proj/piano_inventory/front_end/piano_inventory/index.js
@@ -1,12 +1,20 @@
 // Build a div with text
 
-const webpackInsert = () => {
-    console.log("Component function");
-    const element = document.createElement('div');
-    element.innerHTML = 'Inserting static js file';
-    return element;
-  }
+// const webpackInsert = () => {
+//     console.log("Component function");
+//     const element = document.createElement('div');
+//     element.innerHTML = 'Inserting static js file';
+//     return element;
+//   }
 
-  document.querySelector("#js-insert").appendChild(webpackInsert());
+//   document.querySelector("#js-insert").appendChild(webpackInsert());
   
-console.log("Webpack insert function")
+// console.log("Webpack insert function")
+
+import React from 'react';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <h3>This is a React component rendered in a Django app</h3>,
+  document.getElementById('root')
+);
--- hybrid_piano_proj/webpack.config.js
+++ hybrid_piano_proj/webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
     output: {
         filename: 'indexBundle.js',
         path: path.resolve(__dirname, './piano_inventory/static/piano_inventory'),
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+          },
+        ]
+      }  
 }
